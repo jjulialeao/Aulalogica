c526cd4cf1548500a133dc38e906211d2f5a317a
Juan Pablo dos Santos Passos <jjulialeao@gmail.com>
2026-03-12 12:17:50 -0300

    array

---
+++ atividade_03/lista_compras.js
@@ -0,0 +1,13 @@
+let lista_compras = ["Arroz", "Feijão", "Açúcar", "Salmão"]; 
+lista_compras.push("Macarrão");
+lista_compras.push("Leite");
+lista_compras.push("ovos");
+lista_compras.forEach(item => {
+    console.log(item);
+});
+
+let cesta_frutas = ["Banana", "Maçã", "Laranja", "Abacaxi"];
+cesta_frutas.push("Manga", "Uva", "Melancia");
+cesta_frutas.forEach(fruta => {
+    console.log(fruta);
+});
