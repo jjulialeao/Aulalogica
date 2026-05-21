64dfeb044d8c071c398d2a8afcc4771588fbed6d
leo11-senai <jjulialeao@gmail.com>
2026-05-21 12:13:30 -0300

    rpg,javascript

--- atividade_rpg/javascript.js
+++ atividade_rpg/javascript.js
@@ -7,7 +7,7 @@ class Personagem {
         this.energia = energia;
     }
 }
-class Habiliadades {
+class Habilidade {
     constructor(id, nome, dano, custo, energia) {
         this.id = id;
         this.nome = nome;
@@ -26,8 +26,14 @@ document.getElementById("titulo-heroi").textContent = hero.titulo;
 document.getElementById("nome-boss").textContent = boss.nome;
 document.getElementById("titulo-boss").textContent = boss.titulo;
 // Criar Habilidades
-let listaHabiliadades = [
-    new Habilidade(1, "⚔️ Ataque", 4, 4, 0),
+let containerBtn = document.getElementById("controles");
+let listaHabilidades = [
+    new Habilidade(1, "⚔️ Ataque", 4, 0, 0),
     new Habilidade(2, "🌟 Skill", 8, 10, 0),
     new Habilidade(3, "☄️ Supremo", 15, 0, 100)
 ];
+listaHabilidades.forEach(hab => {
+    let btn = document.createElement("button");//<button>
+    btn.innerText = hab.nome;
+    containerBtn.appendChild(btn);
+}) ;
